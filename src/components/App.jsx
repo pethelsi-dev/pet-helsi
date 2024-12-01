7a7bf71444f2f11b956b8bc88842aa7c7cb3a47f
Victoriia Kyselova <134308356+VictoriaKiselova@users.noreply.github.com>
2024-12-01 03:47:20 +0200

    Save changes before switching branch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,34 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage/HomePage";
+import { lazy, Suspense } from "react";
+import { useSelector } from "react-redux";
+import { selectorIsLoggedIn } from "../redux/auth/selectors";
+import PrivateRoute from "../components/PrivateRoute/PrivateRoute";
+import PublicRoute from "../components/PublicRoute/PublicRoute";
 import style from "./App.module.css";
 
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const LoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
+// const ProfilePage = lazy(() => import("../pages/ProfilePage"));
+const RegisterPage = lazy(() => import("../pages/RegisterPage/RegisterPage"));
+
 export default function App() {
+  const isAuthenticated = useSelector(selectorIsLoggedIn);
+
   return (
     <div className={style.appContainer}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<PublicRoute isAuthenticated={isAuthenticated} />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
+
+          <Route element={<PrivateRoute isAuthenticated={isAuthenticated} />}>
+            {/* <Route path="/profile" element={<ProfilePage />} /> */}
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
