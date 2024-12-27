2059ff83f4bb23b46c2769ce285c9b8d137da232
Halyna Savchenko <galasavchenko1@gmail.com>
2024-12-27 21:51:44 +0100

    add-gif-files

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import decor from "../../assets/Images/vector.png";
+import dog from "../../assets/Images/dog.gif"
 import VeterinarianSearchForm from "../VeterinarianSearchForm/VeterinarianSearchForm";
 import style from "./Hero.module.css";
 
@@ -20,9 +21,7 @@ export default function Hero() {
 
           <div className={style.imageContainer}>
             <img
-              src={
-                "https://s3-alpha-sig.figma.com/img/692a/4ea9/7f0a0a94c9848a2de56708f52966d83d?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fnUvo~dF7ztoZsy~iAUX1~H58Uw9C1wngmS13GIHxrL5-Wm0ebokDcHaktu-BNdOoZFYUl8qFP94LwBBihhTslZZo008SE4bE0e8xJP2CCzx-l06sbKSAzyG3W37uNIu~nOsjiZxj-~Cm-697Auw0QS3KFjyK6BGLXFq2Zbb61IIow6sfkpyWudOVkS0uPN0rQ9FIalr58jvOexHLsycPJ24xQghwPBxr0AKrm~43DSWsJjYj807EqU5Sg1k661wEsLYkMXSPjmLHPjSuD7OmWzSBWTbeUCsjRrPnteMQ-Ze4LSDzypqW7OLvx2lK-gqMwdz4V~ubiggWl-RV4gMmA__"
-              }
+              src={dog}
               alt="Logo"
               className={style.heroDog}
             />
