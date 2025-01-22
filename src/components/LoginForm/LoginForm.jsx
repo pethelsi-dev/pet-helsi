b5d1d2d0dea1d2346dd7849131ba021ba2cb6476
Halyna Savchenko <galasavchenko1@gmail.com>
2025-01-22 17:32:15 +0100

    add styles, add markup

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -7,21 +7,20 @@ import * as Yup from "yup";
 import GoogleAuthorization from "../GoogleAuthorization/GoogleAuthorization";
 import css from "./LoginForm.module.css";
 
-export default function LoginForm({onUserTypeChange}) {
+export default function LoginForm({ onUserTypeChange }) {
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [userType, setUserType] = useState("owner"); 
+  const [userType, setUserType] = useState("owner");
 
   const handleUserTypeChange = (type) => {
     setUserType(type);
     onUserTypeChange(type);
-  }
+  };
 
   const validationSchema = Yup.object({
     email: Yup.string()
       .email("Невірний формат E-mail")
       .required("Введіть е-mail"),
-    password: Yup.string()
-      .required("Введіть пароль"),
+    password: Yup.string().required("Введіть пароль"),
   });
 
   const initialValues = {
@@ -41,18 +40,24 @@ export default function LoginForm({onUserTypeChange}) {
 
   return (
     <div className={css.container}>
-      <h1 className={css.title}>Вхід в <span className={css.span}>PetHelsi</span></h1>
+      <h1 className={css.title}>
+        Вхід в <span className={css.span}>PetHelsi</span>
+      </h1>
       <div className={css.formContainer}>
         <div className={css.buttonContainer}>
           <button
-            className={`${css.switchButton} ${userType === "owner" ? css.activeButton : ""}`}
+            className={`${css.switchButton} ${
+              userType === "owner" ? css.activeButton : ""
+            }`}
             type="button"
             onClick={() => handleUserTypeChange("owner")}
           >
             Я - власник тварини
           </button>
           <button
-            className={`${css.switchButton} ${userType === "doctor" ? css.activeButton : ""}`}
+            className={`${css.switchButton} ${
+              userType === "doctor" ? css.activeButton : ""
+            }`}
             type="button"
             onClick={() => handleUserTypeChange("doctor")}
           >
@@ -69,45 +74,74 @@ export default function LoginForm({onUserTypeChange}) {
             <Form className={css.form}>
               <div className={css.fields}>
                 <div className={css.fieldContainer}>
-                  <label className={css.label} htmlFor="email">E-mail</label>
+                  <label className={css.label} htmlFor="email">
+                    E-mail
+                  </label>
                   <Field
-                    className={`${css.input} ${errors.email && touched.email ? css.inputError : ""}`}
+                    className={`${css.input} ${
+                      errors.email && touched.email ? css.inputError : ""
+                    }`}
                     type="email"
                     name="email"
                     placeholder="Введіть E-mail"
                   />
-                  <ErrorMessage name="email" component="span" className={css.emailError} />
+                  <ErrorMessage
+                    name="email"
+                    component="span"
+                    className={css.emailError}
+                  />
                 </div>
                 <div className={css.fieldContainer}>
-                  <label className={css.label} htmlFor="password">Пароль</label>
-                  <Field
-                    className={`${css.input} ${errors.password && touched.password ? css.inputError : ""}`}
-                    type={passwordVisible ? "text" : "password"}
-                    name="password"
-                    placeholder="Введіть пароль"
-                  />
-                  <button
-                    type="button"
-                    className={css.toggleButton}
-                    onClick={setPasswordVisibleToggler}
-                  >
-                    <SvgIcon
-                      sprite={sprite}
-                      iconName={`${passwordVisible ? "icon-eye_open" : "icon-view_hide"}`}
-                      width="20px"
-                      height="20px"
-                      className={css.icon}
+                  <label className={css.label} htmlFor="password">
+                    Пароль
+                  </label>
+                  <div className={css.inputWrapper}>
+                    <Field
+                      className={`${css.input} ${
+                        errors.password && touched.password
+                          ? css.inputError
+                          : ""
+                      }`}
+                      type={passwordVisible ? "text" : "password"}
+                      name="password"
+                      placeholder="Введіть пароль"
                     />
-                  </button>
-                  <ErrorMessage name="password" component="span" className={css.passwordError} />
+                    <button
+                      type="button"
+                      className={css.toggleButton}
+                      onClick={setPasswordVisibleToggler}
+                    >
+                      <SvgIcon
+                        sprite={sprite}
+                        iconName={`${
+                          passwordVisible ? "icon-eye_open" : "icon-view_hide"
+                        }`}
+                        width="20px"
+                        height="20px"
+                        className={css.icon}
+                      />
+                    </button>
+                  </div>
+                  <ErrorMessage
+                    name="password"
+                    component="span"
+                    className={css.passwordError}
+                  />
                 </div>
                 <div className={css.checkBoxContainer}>
                   <div className={css.rememberMeContainer}>
                     <Field type="checkbox" name="rememberMe" id="rememberMe" />
-                    <label htmlFor="rememberMe" className={css.rememberMeLabel}>Запам'ятати мене</label>
+                    <label htmlFor="rememberMe" className={css.rememberMeLabel}>
+                      Запам'ятати мене
+                    </label>
                   </div>
                   <div className={css.forgotPassword}>
-                    <Link to="/forgot-password" className={css.forgotPasswordLink}>Забули пароль?</Link>
+                    <Link
+                      to="/forgot-password"
+                      className={css.forgotPasswordLink}
+                    >
+                      Забули пароль?
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -128,7 +162,12 @@ export default function LoginForm({onUserTypeChange}) {
           <GoogleAuthorization />
         </div>
       </div>
-      <p className={css.registerNow}>Немає аккаунта? <Link to="/register" className={css.link}>Зареєструйтесь</Link></p>
+      <p className={css.registerNow}>
+        Немає аккаунта?{" "}
+        <Link to="/register" className={css.link}>
+          Зареєструйтесь
+        </Link>
+      </p>
     </div>
   );
 }
