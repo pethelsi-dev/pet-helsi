253bf7f3d64f3f04da2aeddf1bd80d5968cddccf
Halyna Savchenko <galasavchenko1@gmail.com>
2025-01-22 17:49:30 +0100

    add css, markup

--- src/components/RegistrForm/RegistrForm.jsx
+++ src/components/RegistrForm/RegistrForm.jsx
@@ -7,7 +7,7 @@ import * as Yup from "yup";
 import GoogleAuthorization from "../GoogleAuthorization/GoogleAuthorization";
 import css from "./RegistrForm.module.css";
 
-export default function RegistrForm({ onUserTypeChange }) {
+export default function RegistrForm({onUserTypeChange}) {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
   const [userType, setUserType] = useState("owner");
@@ -23,13 +23,10 @@ export default function RegistrForm({ onUserTypeChange }) {
       .required("Введіть е-mail"),
     password: Yup.string()
       .min(8, "Пароль має містити не менше 8 символів - цифри і букви")
-      .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-        "Пароль має містити мінімум одну літеру та одну цифру"
-      )
+      .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Пароль має містити мінімум одну літеру та одну цифру")
       .required("Введіть пароль"),
     confirmPassword: Yup.string()
-      .oneOf([Yup.ref("password"), null], "Паролі повинні співпадати")
+      .oneOf([Yup.ref('password'), null], "Паролі повинні співпадати")
       .required("Повтор пароля є обов'язковим"),
   });
 
@@ -84,7 +81,7 @@ export default function RegistrForm({ onUserTypeChange }) {
           onSubmit={handleSubmit}
           validateOnBlur={true}
         >
-          {({ isSubmitting, errors, touched }) => (
+          {({ isSubmitting, errors, touched}) => (
             <Form className={css.form}>
               <div className={css.fields}>
                 <div className={css.fieldContainer}>
@@ -92,94 +89,68 @@ export default function RegistrForm({ onUserTypeChange }) {
                     E-mail
                   </label>
                   <Field
-                    className={`${css.input} ${
-                      errors.email && touched.email ? css.inputError : ""
-                    }`}
+                    className={`${css.input} ${errors.email && touched.email ? css.inputError : ""}`}
                     type="email"
                     name="email"
                     placeholder="Введіть E-mail"
                   />
-                  <ErrorMessage
-                    name="email"
-                    component="span"
-                    className={css.emailError}
-                  />
+                  <ErrorMessage name="email" component="span" className={css.emailError}/>
                 </div>
                 <div className={css.fieldContainer}>
                   <label className={css.label} htmlFor="password">
                     Пароль
                   </label>
-                  <div className={css.inputWrapper}>
-                    <Field
-                      className={`${css.input} ${
-                        errors.password && touched.password
-                          ? css.inputError
-                          : ""
-                      }`}
-                      type={passwordVisible ? "text" : "password"}
-                      name="password"
-                      placeholder="Введіть пароль"
-                    />
-                    <button
-                      type="button"
-                      className={css.toggleButton}
-                      onClick={setPasswordVisibleToggler}
-                    >
-                      <SvgIcon
-                        sprite={sprite}
-                        iconName={`${
-                          passwordVisible ? "icon-eye_open" : "icon-view_hide"
-                        }`}
-                        width="20px"
-                        height="20px"
-                        className={css.icon}
-                      />
-                    </button>
-                  </div>
-                  <ErrorMessage
+                  <Field
+                    className={`${css.input} ${errors.password && touched.password ? css.inputError : ""}`}
+                    type={passwordVisible ? "text" : "password"}
                     name="password"
-                    component="span"
-                    className={css.passwordError}
+                    placeholder="Введіть пароль"
                   />
+                  <button
+                    type="button"
+                    className={css.toggleButton}
+                    onClick={setPasswordVisibleToggler}
+                  >
+                    <SvgIcon
+                      sprite={sprite}
+                      iconName={`${
+                        passwordVisible ? "icon-eye_open" : "icon-view_hide"
+                      }`}
+                      width="20px"
+                      height="20px"
+                      className={css.icon}
+                    />
+                  </button>
+                  <ErrorMessage name="password" component="span" className={css.passwordError}/>
                 </div>
                 <div className={css.fieldContainer}>
                   <label className={css.label} htmlFor="confirmPassword">
                     Повторіть пароль
                   </label>
-                  <div className={css.inputWrapper}>
-                    <Field
-                      className={`${css.input} ${
-                        errors.confirmPassword && touched.confirmPassword
-                          ? css.inputError
-                          : ""
-                      }`}
-                      type={confirmPasswordVisible ? "text" : "password"}
-                      name="confirmPassword"
-                      placeholder="Повторіть пароль"
-                    />
-                    <button
-                      type="button"
-                      className={css.toggleButton}
-                      onClick={setConfirmPasswordVisibleToggler}
-                    >
-                      <SvgIcon
-                        sprite={sprite}
-                        iconName={`${
-                          confirmPasswordVisible
-                            ? "icon-eye_open"
-                            : "icon-view_hide"
-                        }`}
-                        width="20px"
-                        height="20px"
-                        className={css.icon}
-                      />
-                    </button>
-                  </div>
-                  <ErrorMessage
+                  <Field
+                    className={`${css.input} ${errors.confirmPassword && touched.confirmPassword ? css.inputError : ""}`}
+                    type={confirmPasswordVisible ? "text" : "password"}
                     name="confirmPassword"
-                    component="span"
-                    className={css.confirmPasswordError}
+                    placeholder="Повторіть пароль"
                   />
+                  <button
+                    type="button"
+                    className={css.toggleButton}
+                    onClick={setConfirmPasswordVisibleToggler}
+                  >
+                    <SvgIcon
+                      sprite={sprite}
+                      iconName={`${
+                        confirmPasswordVisible
+                          ? "icon-eye_open"
+                          : "icon-view_hide"
+                      }`}
+                      width="20px"
+                      height="20px"
+                      className={css.icon}
+                    />
+                  </button>
+                  <ErrorMessage name="confirmPassword" component="span" className={css.confirmPasswordError}/>
                 </div>
               </div>
               <div className={css.registrButton}>
